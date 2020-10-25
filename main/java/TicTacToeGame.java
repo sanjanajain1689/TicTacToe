a23e7aa0efa22c350b0c5c2d838aa08b719aa4d9
Sanjana Jain <sanjana.jain04@gmail.com>
2020-10-26 02:53:17 +0530

    Added-UC1_CreatingEmptyBoard

--- main/java/TicTacToeGame.java
+++ main/java/TicTacToeGame.java
@@ -1,5 +1,14 @@
 public class TicTacToeGame {
     public static void main(String args[]) {
         System.out.println("Welcome to Tic Tac Toe Game");
+        char board[] = createBoard();
+    }
+    
+    public static char[] createBoard()
+    {
+        char board[] = new char[10];
+        for(char charValue : board)
+            charValue = ' ';
+        return board;
     }
 }
